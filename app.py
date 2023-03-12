b0d4bb91eb824c12a56592c590e50e179fcef350
sebastian01973 <sebastian.martinez07@uptc.edu.co>
2023-03-11 22:00:28 -0500

    Feat(linear_congruence and multiplicative_congruence): Add methods

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template
 
+from models.linear_congruence import LinearCongruence
+from models.multiplicative_congruence import MultiplicativeCongruence
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():  # put application's code here
+def main(): 
     return render_template('index.html')
 
 
-@app.route('/mean-square')
-def mean_square():
-    return render_template('mean_square.html')
+@app.route('/linear-congruence')
+def linear_congruence(): 
+    linear = LinearCongruence(1,4,3,7)
+    datas = linear.linear_congruence(1,50,10)
+    print(datas)
+    return render_template('linear_congruence.html')
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
