a4f06768f170fa97610a71a5f21733e1e35b9ee8
Jhon-Acevedo <jhon.acevedo01@uptc.edu.co>
2023-03-11 21:59:58 -0500

    Add boostrap

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap5 import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -14,4 +16,4 @@ def mean_square():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ static/js/meanSquare.js
@@ -0,0 +1,6 @@
+const modalParameters = document.getElementById("midSquare")
+
+modalParameters.addEventListener("click", () => {
+    const modal = document.getElementById("myModalB");
+    modal.show();
+}
