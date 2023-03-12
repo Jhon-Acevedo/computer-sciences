f607c9b805dbffc1692bb7b6c34b5e6efce9c94c
Jhon-Acevedo <jhon.acevedo01@uptc.edu.co>
2023-03-12 00:48:31 -0500

    Fix error app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request
 from flask_bootstrap5 import Bootstrap
 
 from models.mean_square import mean_square_implementation
-
+from models.linear_congruence import LinearCongruence
+from models.multiplicative_congruence import MultiplicativeCongruence
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -23,6 +24,29 @@ def mean_square_post():
     data = mean_square_implementation(request.form['seed'], (request.form['minimum']), request.form['maximum'],
                                       request.form['iterations'])
     return render_template('mean_square.html', data=data)
+
+
+
+@app.route('/linear-congruence')
+def linear_congruence():
+    return render_template('linear_congruence.html')
+
+
+@app.route('/linear-congruence', methods=["POST"])
+def linear_congruence_data():
+    context = {
+        'x0': int(request.form.get('x0')),
+        'k': int(request.form.get('k')),
+        'c': int(request.form.get('c')),
+        'g': int(request.form.get('g')),
+        'minimum': int(request.form.get('minimum')),
+        'maximum': int(request.form.get('maximum')),
+        'iterations': int(request.form.get('iterations')),
+    }
+    linear = LinearCongruence(context['x0'], context['k'], context['g'], context['g'])
+    datas = linear.linear_congruence(context['minimum'], context['maximum'], context['iterations'])
+    return render_template('linear_congruence.html', datas=datas)
+
 
 
 if __name__ == '__main__':
